2cf5218e80c31437d08a962d7ca477605be10861
shuduhadi <shuduhadi18@gmail.com>
2026-06-05 18:16:33 +0200

    feature: Implemted PBKDF2 key derivation and salt generation

--- tests/test_encryption.py
+++ tests/test_encryption.py
@@ -1,6 +1,6 @@
 import pytest
 import bcrypt
-from encryption import hash_master_password, verify_master_password
+from encryption import hash_master_password, verify_master_password, generate_salt, derive_key
 
 def test_hash_returns_bytes():
     result = hash_master_password("mypassword")
@@ -22,3 +22,31 @@ def test_hash_different_each_time():
     hash1 = hash_master_password("mypassword")
     hash2 = hash_master_password("mypassword")
     assert hash1 != hash2
+
+def test_generate_salt_returns_bytes():
+    salt = generate_salt()
+    assert isinstance(salt,bytes)
+
+def test_generate_salt_length():
+    salt = generate_salt()
+    assert len(salt) == 16
+
+def test_same_password_same_salt_same_key():
+    salt = generate_salt()
+
+    key1 = derive_key("mypassword", salt)
+    key2 = derive_key("mypassword", salt)
+    assert key1 == key2
+
+def test_diff_salts_diff_keys():
+    salt1 = generate_salt()
+    salt2 = generate_salt()
+
+    key1 = derive_key("mypassword", salt1)
+    key2 = derive_key("mypassword", salt2)
+    assert key1 != key2
+
+def test_key_is_44_characters():
+    salt = generate_salt()
+    key = derive_key("mypassowrd", salt)
+    assert len(key) == 44
